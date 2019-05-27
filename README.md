5f469e2adb25f0c41af1d324b066210cdc944e9c
Raushan Jha <raushanJha465@gmail.com>
2019-05-27 18:35:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Shimmer Effect  App in Android Studio
+
+
+# Show some ❤️ and star the repo to support the project
+
+  - fork this repo and see the code
+### Development
+
+Want to contribute? Great!
+
+Designed And Developed By 
+----
+Raushan Jha
+
+# ScreenShots
+ <img src="/screenshot/1.jpeg" width="400px"/>
---
+++ app/src/main/java/com/example/shimmereffectexample/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.shimmereffectexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    ShimmerFrameLayout mShimmerViewContainer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShimmerViewContainer = findViewById(R.id.shimmer_view_container);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mShimmerViewContainer.startShimmerAnimation();
+    }
+
+    @Override
+    public void onPause() {
+        mShimmerViewContainer.stopShimmerAnimation();
+        super.onPause();
+    }
+}
